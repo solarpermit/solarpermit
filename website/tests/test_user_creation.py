6734c7054030d88c0dfff7443197713bd8c3730a
Daniel Brooks <db48x@db48x.net>
2013-12-27 18:26:35 -0500

    test the creation of new users
    runs the ajax call, then checks that the new user isn't
    staff/superuser, and can log in.

---
+++ website/tests/test_user_creation.py
@@ -0,0 +1,29 @@
+from django.utils import unittest
+from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+import inspect
+
+class NewUserTestCase(TestCase):
+    def test_create_user(self):
+        client = Client();
+        res = client.post('/account/',
+                          { 'ajax': "create_account_submit",
+                            'invitation_key': "",
+                            'email': "create@testing.solarpermit.org",
+                            'title': "Engineer",
+                            'firstname': "abc",
+                            'lastname': "def",
+                            'username': "abcdef",
+                            'display_as': "realname",
+                            'password': "qwertyuiop",
+                            'verify_password': "qwertyuiop" })
+        self.assertEqual(res.status_code, 200)
+        new_user = User.objects.all()[0]
+        self.assertFalse(new_user.is_superuser)
+        self.assertFalse(new_user.is_staff)
+        auth_user = authenticate(username = 'abcdef',
+                                 password = 'qwertyuiop')
+        self.assertIsNotNone(auth_user);
+        self.assertEqual(new_user, auth_user)
