fecaedd6a453838f47d1e10812f5c392146c2dcf
Daniel Brooks <db48x@db48x.net>
2014-02-20 17:49:28 -0800

    show the coverage report for every question
    This means that most questions show two tables and two graphs.

--- website/views/reporting.py
+++ website/views/reporting.py
@@ -83,51 +83,76 @@ def yes_no_exception_field(field_name):
                         ("Total", total())])
 
 reports_by_type = {
-    "available_url_display.html": yes_no_field("available"),
-    "radio_with_exception_display.html": yes_no_exception_field("required"),
-    "plan_check_service_type_display.html": OrderedDict([("Over the Counter", json_match("plan_check_service_type", "over the counter")),
-                                                         ("In-House (not same day)", json_match("plan_check_service_type", "in-house")),
-                                                         ("Outsourced", json_match("plan_check_service_type", "outsourced")),
-                                                         ("Total", total())]),
-    "radio_compliant_sb1222_with_exception.html": yes_no_exception_field("compliant"),
-    "inspection_checklists_display.html": yes_no_field("value"),
-    "radio_has_training_display.html": yes_no_field("value"),
-    "phone_display.html": coverage_report(),
-    "url.html": coverage_report(),
-    "address_display.html": coverage_report(),
-    "radio_submit_PE_stamped_structural_letter_with_exception_display.html": yes_no_exception_field("required"),
-    "hours_display.html": coverage_report(),
-    "turn_around_time_display.html": coverage_report(), # check spec, can do more here
-    "available_url_display.html": coverage_report(),
-    "permit_cost_display.html": coverage_report(), # check the spec, probably needs histograms and stuff
-    "radio_required_for_page_sizes_display.html": yes_no_field("required"), # should do more for the required values
-    "radio_required_for_scales_display.html": yes_no_field("required"), # likewise
-    "radio_required_display.html": yes_no_field("required"),
-    "radio_covered_with_exception_display.html": yes_no_exception_field("required"),
-    "radio_studer_vent_rules_with_exception_display.html": yes_no_exception_field("allowed"),
-    "radio_module_drawings_display.html": OrderedDict([("Yes", json_match("value", "must draw individual modules")),
-                                                       ("No", json_match("value", "n in series in a rectangle allowed")),
-                                                       ("Total", total())]),
-    "radio_allowed_with_exception_display.html": yes_no_exception_field("allowed"),
-    "required_spec_sheets_display.html": coverage_report(),
-    "homeowner_requirements_display.html": coverage_report(), # two yes/no answers in one
-    "fire_setbacks_display.html": yes_no_exception_field("enforced"),
-    "radio_inspection_approval_copies_display.html": OrderedDict([("In person", json_match("apply", "in person")),
-                                                                  ("Remotely", json_match("apply", "remotely")),
-                                                                  ("Total", total())]),
-    "signed_inspection_approval_delivery_display.html": coverage_report(),
-    "radio_vent_spanning_rules_with_exception_display.html": yes_no_exception_field("allowed"),
-    "solar_permitting_checklists_display.html": coverage_report(),
-    "radio_available_with_exception_display.html": yes_no_exception_field("available"),
-    "time_window_display.html": OrderedDict([("Exact time given", json_match("time_window", "0")),
-                                             ("2 hours (or less)", json_match("time_window", "2")),
-                                             ("Half Day (2 to 4 hours)", json_match("time_window", "4")),
-                                             ("Full Day (greater than 4 hours)", json_match("time_window", "8")),
-                                             ("Total", total())]),
-    "radio_has_training_display.html": yes_no_field("value"),
-    "radio_licensing_required_display.html": yes_no_field("required"),
-    "online_forms.html": coverage_report(),
-    None: coverage_report()
+    "available_url_display.html": [coverage_report(), yes_no_field("available")],
+    "radio_with_exception_display.html": [coverage_report(), yes_no_exception_field("required")],
+    "plan_check_service_type_display.html": [coverage_report(),
+                                             OrderedDict([("Over the Counter",
+                                                           json_match("plan_check_service_type",
+                                                                      "over the counter")),
+                                                          ("In-House (not same day)",
+                                                           json_match("plan_check_service_type",
+                                                                      "in-house")),
+                                                          ("Outsourced",
+                                                           json_match("plan_check_service_type",
+                                                                      "outsourced")),
+                                                          ("Total", total())])],
+    "radio_compliant_sb1222_with_exception.html": [coverage_report(),
+                                                   yes_no_exception_field("compliant")],
+    "inspection_checklists_display.html": [coverage_report(),
+                                           yes_no_field("value")],
+    "radio_has_training_display.html": [coverage_report(),
+                                        yes_no_field("value")],
+    "phone_display.html": [coverage_report()],
+    "url.html": [coverage_report()],
+    "address_display.html": [coverage_report()],
+    "radio_submit_PE_stamped_structural_letter_with_exception_display.html": [coverage_report(),
+                                                                              yes_no_exception_field("required")],
+    "hours_display.html": [coverage_report()],
+    "turn_around_time_display.html": [coverage_report()], # check spec, can do more here
+    "available_url_display.html": [coverage_report()],
+    "permit_cost_display.html": [coverage_report()], # check the spec, probably needs histograms and stuff
+    "radio_required_for_page_sizes_display.html": [coverage_report(),
+                                                   yes_no_field("required")], # should do more for the required values
+    "radio_required_for_scales_display.html": [coverage_report(),
+                                               yes_no_field("required")], # likewise
+    "radio_required_display.html": [coverage_report(),
+                                    yes_no_field("required")],
+    "radio_covered_with_exception_display.html": [coverage_report(),
+                                                  yes_no_exception_field("required")],
+    "radio_studer_vent_rules_with_exception_display.html": [coverage_report(),
+                                                            yes_no_exception_field("allowed")],
+    "radio_module_drawings_display.html": [coverage_report(),
+                                           OrderedDict([("Yes",
+                                                         json_match("value",
+                                                                    "must draw individual modules")),
+                                                        ("No",
+                                                         json_match("value",
+                                                                    "n in series in a rectangle allowed")),
+                                                        ("Total", total())])],
+    "radio_allowed_with_exception_display.html": [coverage_report(),
+                                                  yes_no_exception_field("allowed")],
+    "required_spec_sheets_display.html": [coverage_report()],
+    "homeowner_requirements_display.html": [coverage_report()], # two yes/no answers in one
+    "fire_setbacks_display.html": [coverage_report(),
+                                   yes_no_exception_field("enforced")],
+    "radio_inspection_approval_copies_display.html": [coverage_report(),
+                                                      OrderedDict([("In person", json_match("apply", "in person")),
+                                                                   ("Remotely", json_match("apply", "remotely")),
+                                                                   ("Total", total())])],
+    "signed_inspection_approval_delivery_display.html": [coverage_report()],
+    "radio_vent_spanning_rules_with_exception_display.html": [coverage_report(), yes_no_exception_field("allowed")],
+    "solar_permitting_checklists_display.html": [coverage_report()],
+    "radio_available_with_exception_display.html": [coverage_report(), yes_no_exception_field("available")],
+    "time_window_display.html": [coverage_report(),
+                                 OrderedDict([("Exact time given", json_match("time_window", "0")),
+                                              ("2 hours (or less)", json_match("time_window", "2")),
+                                              ("Half Day (2 to 4 hours)", json_match("time_window", "4")),
+                                              ("Full Day (greater than 4 hours)", json_match("time_window", "8")),
+                                              ("Total", total())])],
+    "radio_has_training_display.html": [coverage_report(), yes_no_field("value")],
+    "radio_licensing_required_display.html": [coverage_report(), yes_no_field("required")],
+    "online_forms.html": [coverage_report()],
+    None: [coverage_report()]
 }
 
 reports_by_qid = {
@@ -184,18 +209,21 @@ def report_on(request, question_id):
     data['current_nav'] = 'reporting'
     data['report_name'] = question.question
     data['question_instruction'] = question.instruction
-    report = (question.id in reports_by_qid and reports_by_qid[question_id]) or (question.display_template in reports_by_type and reports_by_type[question.display_template])
-    query = build_query(question, report)
-    print(query)
-    cursor = connection.cursor()
-    cursor.execute(query)
-    #print cursor.fetchall()
-    result = dict(zip([col[0] for col in cursor.description], cursor.fetchone()))
+    reports = (question.id in reports_by_qid and reports_by_qid[question_id]) or \
+              (question.display_template in reports_by_type and reports_by_type[question.display_template])
 
-    data['table'] = []
-    for key in report.keys():
-        data['table'].append({'key': key, 'value': result[key]})
-    data['table_json'] = json.dumps(data['table'])
+    data['reports'] = []
+    idx = 0
+    for report in reports:
+      query = build_query(question, report)
+      print(query)
+      cursor = connection.cursor()
+      cursor.execute(query)
+      table = [{'key': k, 'value': v } for (k,v) in zip([col[0] for col in cursor.description], cursor.fetchone())]
+      data['reports'].append({ "idx": idx,
+                               "table": table })
+      idx += 1
+    data['reports_json'] = json.dumps(data['reports'])
 
     requestProcessor = HttpRequestProcessor(request)
     return requestProcessor.render_to_response(request,'website/reporting/report_on.html', data, '')
