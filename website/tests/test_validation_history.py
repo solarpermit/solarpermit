9ef79d6a1033ed2c4a65ed9679a40cb61269fc4e
Therianulf <ben.larson47@gmail.com>
2014-03-03 20:26:11 +0000

    Finished overview, Reachable goal in mind. Coding commencing

--- website/tests/test_validation_history.py
+++ website/tests/test_validation_history.py
@@ -14,20 +14,32 @@ cron_validate_answers
 
 function setup
 Create sample jurisdiction
-create sample 3 questions for approved, pending, rejected
-create sample answers
-superuser add answer
+Create sample question for:
+        approved with downvotes
+        approved without downvotes
+        approved with no votes
+        approved multi value with downvotes
+        approved multi value without downvotes
+        rejected with downvotes
+        rejected with downvotes and upvotes
+        rejected multi value with downvotes
+        rejected multi value with downvotes and upvotes
+        rejected after already approved?? I don't think i need to test this feature.
+        
+    create sample answers (multianswer questions will have 3 answers, pending, approved, reject)
+    superuser add answer?? waiting for db48x to reply.
 
 function vote
-down vote answer to limit to reject
-upvote answer to limit to approve
-leave answer pending
+    downvote answer to 2 to reject
+    upvote answer to 1 to approve
+    downvote answer, upvote to downvote + 1 to approve
+    leave answer pending for week to approve
 
 function timepass
-simulate days passing
-call cron script every simulated day.
-assert answered questions against template
-log errors
+    simulate days passing
+    call cron script every simulated day.
+    assert answered questions against template
+    log errors
 
 '''
 class TestValidHistory(TestCase):
