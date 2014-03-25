ebec58d2b2970b193c85bf099bef38bb31a2d5a4
Daniel Brooks <db48x@db48x.net>
2014-03-25 11:44:28 -0700

    autocomplete for counties/cities should not require a prefix match

--- website/views/autocomp.py
+++ website/views/autocomp.py
@@ -5,13 +5,13 @@ from autocomplete.views import AutocompleteView, AutocompleteSettings
 autocomplete_instance = AutocompleteView('website')
 class CountiesSettings(AutocompleteSettings):
     queryset = Jurisdiction.objects.filter(jurisdiction_type__in = ('CO', 'SC', 'CC'))
-    search_fields = ['^name']
+    search_fields = ['name']
     key = 'pk'
     label = Jurisdiction.get_jurisdiction_display
 autocomplete_instance.register("counties", CountiesSettings)
 class CitiesSettings(AutocompleteSettings):
     queryset = Jurisdiction.objects.filter(jurisdiction_type__in = ('CC', 'CI', 'IC'))
-    search_fields = ['^name']
+    search_fields = ['name']
     key = 'pk'
     label = Jurisdiction.get_jurisdiction_display
 autocomplete_instance.register("cities", CitiesSettings)
