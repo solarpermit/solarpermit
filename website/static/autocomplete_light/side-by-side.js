a41a7e7ccef511ea2387e54bb196a53e07f3a7b2
Daniel Brooks <db48x@db48x.net>
2014-04-20 07:32:53 -0700

    try to avoid large queries during autocomplete
    With this change we don't start searching until the user has typed a
    minimum of four characters, unless the user has entered one of the 32
    most common four-character substrings, in which case we wait for a
    fifth character. These 32 substrings all return more than 200 results,
    and in some cases thousands. Result sets that large are quite
    difficult to navigate, and take an inordinate amount of time to
    retrieve and insert into the page. I'm sure we can speed things up,
    but for now this will help the most for the least effort.

--- website/static/autocomplete_light/side-by-side.js
+++ website/static/autocomplete_light/side-by-side.js
@@ -1,3 +1,12 @@
+// These substrings get you hundreds or thousands of results so we
+// avoid searching for them, opting instead to wait for the user to
+// type another character
+var COMMON = ["ille", "vill", "land", "owns", "svil", "ship", "nshi",
+              "wnsh", "ield", "fiel", "burg", "ngto", "ingt", "gton",
+              "ford", "ring", "town", "sbur", "ston", "wood", "ount",
+              "ster", "orth", "prin", "nton", "ings", "lton", "nvil",
+              "dale", "idge", "reen", "outh"];
+
 function nothing() {
     // do nothing, on purpose
 }
@@ -5,14 +14,26 @@ var ourWidget = { initializeAutocomplete: function () {
                       this.select = this.widget.find('select.value-select');
                       var box = this.box = this.widget.find('select.box'); 
                       var deck = this.deck = this.widget.find('select.sbsdeck');
-                      this.autocomplete = this.input.yourlabsAutocomplete({ box: this.box,
-                                                                            fixPosition: nothing,
-                                                                            hide: nothing,
-                                                                            move: nothing,
-                                                                            boxMouseenter: nothing,
-                                                                            boxMouseleave: nothing,
-                                                                            boxClick: nothing
-                                                                          });
+                      this.autocomplete = this.input
+                                              .yourlabsAutocomplete({ box: this.box,
+                                                                      fixPosition: nothing,
+                                                                      hide: nothing,
+                                                                      move: nothing,
+                                                                      boxMouseenter: nothing,
+                                                                      boxMouseleave: nothing,
+                                                                      boxClick: nothing,
+                                                                      minimumCharacters: 4,
+                                                                      refresh: function () {
+                                                                                   this.value = this.getQuery();
+                                                                                   if (this.value.length > 4 ||
+                                                                                       COMMON.indexOf(this.value) === -1) {
+                                                                                       yourlabs.Autocomplete
+                                                                                               .prototype
+                                                                                               .refresh
+                                                                                               .call(this);
+                                                                                   }
+                                                                               }
+                                                                    });
                       var self = this;
                       $("button.select").click(function () {
                                                    box.find(':selected')
